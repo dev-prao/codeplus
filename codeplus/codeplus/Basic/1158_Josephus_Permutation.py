bb28a53f09ee6c040083c9baf6493f314ebd0caa
prao <pch5521@gmail.com>
2023-03-21 01:00:30 +0900

    deque

--- codeplus/codeplus/Basic/1158_Josephus_Permutation.py
+++ codeplus/codeplus/Basic/1158_Josephus_Permutation.py
@@ -3,10 +3,10 @@ from collections import deque
 n, k = map(int, input().split())  # N은 총 사람 수, K번째 사람 제거
 q = deque()
 for i in range(1, n + 1):
-    q.append(i)
+    q.append(i) # 1 2 3 4 5 6 7
 ans = []
 for i in range(n - 1):
-    for j in range(k - 1):
+    for j in range(k - 1): #  3 4 5 6 7 1 2
         q.append(q.popleft())
     ans += [q.popleft()]
 
