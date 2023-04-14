ed7fb693a9db296072dc231a64239d8d71f032c1
prao <pch5521@gmail.com>
2023-04-14 18:21:41 +0900

    2xn 타일링

---
+++ codeplus/codeplus/Basic/11727_2xn_Tiling2.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def tiling2(n):
+    dp = [0] * 1001
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
+    return dp[n]
+
+
+n = int(input())
+print(tiling2(n))
